98af68c53777040a7bb3ed4e4ff7df524ad12cff
IgnisGloriam <128818807+IgnisGloriam@users.noreply.github.com>
2024-06-02 13:23:02 +0300

    Added multiMemory
    For each users bot contains unique dialogue

---
+++ gigaLiterature/gigaLiterature.py
@@ -0,0 +1,58 @@
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
+bot = AsyncTeleBot('') #TelegramToken
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.chat_models.gigachat import GigaChat
+
+
+# Авторизация в сервисе GigaChat
+chat = GigaChat(credentials='', scope='GIGACHAT_API_PERS', verify_ssl_certs=False) #GigaChatToken(ends with Q==)
+
+
+
+user_requests = {}
+initial_request = SystemMessage(
+        content="Ты - литературовед. Ты умный бот-литературовед, который помогает пользователю найти интересные книги! Тебя зовут gigaLiterature"
+    )
+
+def add_user(user_id):
+    if user_id not in user_requests:
+        user_requests[user_id] = {
+
+            'requests': [initial_request]
+        }
+
+
+
+def add_request(user_id, new_request):
+    if user_id not in user_requests:
+        add_user(user_id)
+
+    user_requests[user_id]['requests'].append(new_request)
+
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, """\
+Привет, я умный бот-литературовед, который помогает пользователю найти интересные книги!\
+Чем я могу вам помочь?
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    userId = message.from_user.id
+    add_request(userId, HumanMessage(content=message.text))
+    #print(userId, 'USER: ', message.text)
+    res = chat(user_requests[userId]['requests'])
+
+    add_request(userId, res)
+    #print(userId, 'BOT: ', res.content)
+    print(user_requests)
+    await bot.reply_to(message, res.content)
+
+
+asyncio.run(bot.polling())
